0b96554b157ee0c60ad1d78d9ee9e2d6ac4f66fa
rolmos14 <olmos.rafa@gmail.com>
2021-07-31 20:43:01 +0200

    Stage 2/5. Created class to manage the game and documented with docstrings.

--- Simple Tic-Tac-Toe/task/tictactoe.py
+++ Simple Tic-Tac-Toe/task/tictactoe.py
@@ -1,5 +1,44 @@
-print("""
-X O X
-O X O
-X X O
-""")
+class TicTacToe:
+    """
+    A class to represent TicTacToe game.
+
+    ...
+
+    Attributes
+    ----------
+        grid : list
+            contains the cells of the game and their current status.
+            Accepted symbols are 'X', 'O' and '_'
+    """
+
+    def __init__(self):
+        """The constructor to initialize the object."""
+        self.grid = []
+
+    def update(self, cells):
+        """
+        Updates the grid with the new status passed in argument 'cells'.
+
+        Parameters
+        ----------
+        cells : str
+            Status of all the cells of the game.
+            Accepted symbols are 'X', 'O' and '_'
+        """
+        self.grid = list(cells)
+
+    def __str__(self):
+        """
+        Return a representation of the current status of the game.
+        """
+        line_of_dashes = 9 * "-"
+        row_1 = f"| {self.grid[0]} {self.grid[1]} {self.grid[2]} |"
+        row_2 = f"| {self.grid[3]} {self.grid[4]} {self.grid[5]} |"
+        row_3 = f"| {self.grid[6]} {self.grid[7]} {self.grid[8]} |"
+        return line_of_dashes + "\n" + row_1 + "\n" + row_2 + "\n" + row_3 + "\n" + line_of_dashes
+
+
+game = TicTacToe()
+cells = input("Enter cells: ")
+game.update(cells)
+print(game)
